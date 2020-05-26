aa9979ffd351d3ce15b576bbc25da01004adbb47
Pergamene <smartz373@gmail.com>
2020-05-26 13:39:42 -0600

    set up token authentication with okta library instead of passport

--- src/api/server.js
+++ src/api/server.js
@@ -3,14 +3,13 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
-const passport = require('../utils/Passport');
+const authenticationRequired = require('../utils/OktaAuth');
 
 //Routes
 const UserRouter = require('../routes/Users');
 
 const server = express();
 
-server.use(passport.initialize());
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
@@ -20,7 +19,7 @@ server.use('/api/users', UserRouter);
 //Root Route
 server.get('/', (req, res) => res.send('Server is active'));
 
-server.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+server.post('/', authenticationRequired, (req, res) => {
   console.log('in post');
 });
 
