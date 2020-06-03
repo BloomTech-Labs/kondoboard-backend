bcfa1a469a1abf4776b6fae979d27e0d242560b6
Teague <teaguehannam@gmail.com>
2020-06-03 10:27:55 -0600

    fixed current tests

--- src/routes/test/DataScience.spec.js
+++ src/routes/test/DataScience.spec.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const request = require('supertest');
-const server = require('../api/server');
-const db = require('../database/dbConfig');
+const server = require('../../api/server');
+const db = require('../../database/dbConfig');
 
 describe('DSRouter', () => {
   beforeEach(async () => {
