8c43db73860738dcd77aeb8b3d9f06a8783117e2
Laien Xie <angela@Laiens-MacBook-Air.local>
2020-06-03 12:17:21 -0700

    updated db

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 const server = require('./src/api/server');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5080;
 
 server.listen(PORT, () => {
 	console.log(`\n\n ~~~ Server listening on port ${PORT} ~~~\n\n`);
